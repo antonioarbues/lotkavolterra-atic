265b87eb2613fc1640240b86a5f6eb276fa13ad9
Antonio <antonio.arbues99@gmail.com>
2021-05-28 18:44:26 +0200

    vector field implemented also for decoupled

--- src/Dynamics.py
+++ src/Dynamics.py
@@ -10,8 +10,11 @@ class Dynamics:
         self.config = config.params
         self.dt = self.config['dt']
         self.x0, self.y0, self.z0, self.w0 = self.setInitialConditions()
+        self.dx, self.dy, self.dz, self.dw = 0, 0, 0, 0
         self.alpha1, self.beta1, self.gamma1, self.delta1, \
             self.alpha2, self.beta2, self.gamma2, self.delta2 = self.setParameters()
+        self.params1 = [self.alpha1, self.beta1, self.gamma1, self.delta1]
+        self.params2 = [self.alpha2, self.beta2, self.gamma2, self.delta2]
         self.isFirstIteration = True
         self.plotter = Plotter()
 
@@ -23,17 +26,23 @@ class Dynamics:
         y1 = y0 + self.dt * (gamma * y0 - delta * x0 * y0)
         return x1, y1
 
+    def f(self, x0, y0, params):
+        '''
+        returns the dynamics of the system
+        '''
+        alpha = params[0]
+        beta = params[1]
+        gamma = params[2]
+        delta = params[3]
+        return alpha * x0 - beta * x0 * y0, gamma * y0 - delta * x0 * y0
+
     def updateRK4(self, x0, y0, alpha, beta, gamma, delta):
         dt = self.dt
-        def f(x0, y0):
-            '''
-            returns the dynamics of the system
-            '''
-            return alpha * x0 - beta * x0 * y0, gamma * y0 - delta * x0 * y0 
-        k1x, k1y = f(x0, y0)
-        k2x, k2y = f(x0 + 0.5 * dt * k1x, y0 + 0.5 * dt * k1y)
-        k3x, k3y = f(x0 + 0.5 * dt * k2x, y0 + 0.5 * dt * k2y)
-        k4x, k4y = f(x0 + dt * k3x, y0 + dt * k3y)
+        params = [alpha, beta, gamma, delta]
+        k1x, k1y = self.f(x0, y0, params)
+        k2x, k2y = self.f(x0 + 0.5 * dt * k1x, y0 + 0.5 * dt * k1y, params)
+        k3x, k3y = self.f(x0 + 0.5 * dt * k2x, y0 + 0.5 * dt * k2y, params)
+        k4x, k4y = self.f(x0 + dt * k3x, y0 + dt * k3y, params)
         x1 = x0 + (1/6) * dt * (k1x + 2 * k2x + 2 * k3x + k4x)
         y1 = y0 + (1/6) * dt * (k1y + 2 * k2y + 2 * k3y + k4y)
         return x1, y1
@@ -43,16 +52,17 @@ class Dynamics:
         '''
         returns the updated x1, y1, z1, w1
         '''
-        self.plotter.updatePlotterBuffer(self.x0, self.y0, self.z0, self.w0)
+        self.dx, self.dy = self.f(self.x0, self.y0, self.params1)
+        self.dz, self.dw = self.f(self.z0, self.w0, self.params2)
         if self.config['useRK4']:
             self.x0, self.y0 = self.updateRK4(self.x0, self.y0, self.alpha1, self.beta1, self.gamma1, self.delta1)
             self.z0, self.w0 = self.updateRK4(self.z0, self.w0, self.alpha2, self.beta2, self.gamma2, self.delta2)
-            return self.x0, self.y0, self.z0, self.w0
 
         elif self.config['useEulerForward']:
             self.x0, self.y0 = self.updateLVEuler(self.x0, self.y0, self.alpha1, self.beta1, self.gamma1, self.delta1)
             self.z0, self.w0 = self.updateLVEuler(self.z0, self.w0, self.alpha2, self.beta2, self.gamma2, self.delta2)
-            return self.x0, self.y0, self.z0, self.w0
+        
+        self.plotter.updatePlotterBuffer(self.x0, self.y0, self.z0, self.w0, self.dx, self.dy, self.dz, self.dw)
 
     def setInitialConditions(self):
         return self.config['x0'], self.config['y0'], self.config['z0'], self.config['w0']
